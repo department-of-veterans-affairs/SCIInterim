f35e94ea130408b8869f94cd7520ceadb598137c
Albert J. Wong <omb.awong@gmail.com>
2015-04-25 07:48:03 -0700

    Organize fields a little more

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,26 +2,43 @@ class Patient < ActiveRecord::Base
   def self.to_display_name(symbol)
     return symbol.to_s.split('_').join(' ').titleize
   end
+
   def self.fields
+    self.basic_fields + self.detailed_fields + self.assessment_fields
+  end
+
+  def self.basic_fields
     [
       [ :patient_id, :integer ],
       [ :name, :string ],
       [ :ssn, :string ],
       [ :dob, :datetime ],
-      [ :asia_level, :string ],
-      [ :asia_impairment, :string ],
-      [ :bladder_drainage, :string ],
       [ :employment_status, :string ],
       [ :highest_level_of_education, :string ],
       [ :registration_status, :string ],
       [ :sci_network, :string ],
-      [ :sci_network_date_changed, :datetime ],
+      [ :date_of_death, :datetime ],
+      [ :outcome_coordinator, :string ],
+    ]
+  end
+
+  def self.assessment_fields
+    [
+      [ :asia_level, :string ],
+      [ :asia_impairment, :string ],
+      [ :bladder_drainage, :string ],
       [ :type_of_etiology, :string ],
       [ :etiology, :string ],
       [ :other_etiology, :string ],
       [ :date_of_onset, :datetime ],
       [ :brain_injury, :string ],
       [ :other_injury, :string ],
+    ]
+  end
+
+  def self.detailed_fields
+    [
+      [ :sci_network_date_changed, :datetime ],
       [ :annual_eval_vamc, :string ],
       [ :primary_care_vamc, :string ],
       [ :additional_care_vamc, :string ],
@@ -33,8 +50,6 @@ class Patient < ActiveRecord::Base
       [ :data_first_seen_in_va_sci, :string ],
       [ :occupation_at_time_of_injury, :string ],
       [ :service_connected, :string ],
-      [ :date_of_death, :datetime ],
-      [ :outcome_coordinator, :string ],
     ]
   end
 end
