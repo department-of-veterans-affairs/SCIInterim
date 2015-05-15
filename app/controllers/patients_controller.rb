71b156bd91e84b3f82eaeb5494982d1c4b3d6a15
mrjadaml <mrjadaml@gmail.com>
2015-05-15 14:50:47 -0700

    renders edit view only after #create #update and #index

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -24,12 +24,12 @@ class PatientsController < ApplicationController
   def create
     @patient = Patient.new(patient_params)
     @patient.save
-    respond_with(@patient)
+    render :edit
   end
 
   def update
     @patient.update(patient_params)
-    respond_with(@patient)
+    render :edit
   end
 
   def destroy
