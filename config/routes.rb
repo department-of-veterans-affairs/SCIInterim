806c172eabec0233824fad41805b5ee5655c42f6
Albert J. Wong <omb.awong@gmail.com>
2015-05-20 14:53:36 -0700

    Hack for demo

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,22 @@ Rails.application.routes.draw do
   get 'reports/entity/:entity/:entity_id'            => 'reports#entity_reports'
   get 'reports/entity/:entity/:entity_id/:report_id' => 'reports#run_report'
 
+    resources :acute_rehab1_years
+    resources :acute_rehab90_days
+    resources :acute_rehabs
+    resources :annual_evaluations
+    resources :omr1_years
+    resources :omr90_days
+    resources :omrs
+
   resources :patients do
-    resources :acute_rehab1_years, except: [:index, :show, :destroy]
-    resources :acute_rehab90_days, except: [:index, :show, :destroy]
-    resources :acute_rehabs, except: [:index, :show, :destroy]
-    resources :annual_evaluations, except: [:index, :show, :destroy]
-    resources :omr1_years, except: [:index, :show, :destroy]
-    resources :omr90_days, except: [:index, :show, :destroy]
-    resources :omrs, except: [:index, :show, :destroy]
+    resources :acute_rehab1_years, except: [:index, :destroy]
+    resources :acute_rehab90_days, except: [:index, :destroy]
+    resources :acute_rehabs, except: [:index, :destroy]
+    resources :annual_evaluations, except: [:index, :destroy]
+    resources :omr1_years, except: [:index, :destroy]
+    resources :omr90_days, except: [:index, :destroy]
+    resources :omrs, except: [:index, :destroy]
   end
 
   root to: redirect('/patients')
