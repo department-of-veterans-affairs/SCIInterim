e4f5b53c95576d568928dcd1c3a2a6d3fb44bb76
Albert J. Wong <awong.dev@gmail.com>
2015-06-17 01:43:16 -0700

    Remove redundant manual form class specifications
    The need to manually annotate form-control, horizontal_input_group,
    etc., is an artifact of previously not understanding simple_form
    wrappers. Now that the wrappers are correctly set up, these manual
    annotations are superfluous and only serve to make the code harder to
    read. Remove all the superfluous stuff.
    
    Also use the jquery date picker instead of the built-in html5 version
    because our target IE9 almost certainly doesn't understand html5 at that
    level.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
 //= require_tree .
 
-
+$(function() {
+      $('input.datepicker').data({behaviour: "datepicker"}).datepicker();
+});
