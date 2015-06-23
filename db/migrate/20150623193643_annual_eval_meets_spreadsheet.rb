51ac525a74bc18b80247705a33187fea82eccdf3
Albert J. Wong <awong.dev@gmail.com>
2015-06-23 14:06:16 -0700

    Match physical schema with Annual Eval Spreadsheet
      * Rename fields where possible
      * Ensure the column types are correct
      * Delete unused columns.

---
+++ db/migrate/20150623193643_annual_eval_meets_spreadsheet.rb
@@ -0,0 +1,36 @@
+class AnnualEvalMeetsSpreadsheet < ActiveRecord::Migration
+  def change
+    change_column :annual_evaluations, :bmi, 'integer USING CAST(bmi AS integer)'
+    change_column :annual_evaluations, :bladder_drainage_method, 'integer USING CAST(bladder_drainage_method AS integer)'
+    add_column :annual_evaluations, :has_motor_or_sensory_asymmetry, :boolean
+    add_column :annual_evaluations, :neuro_zone_of_preservation1, :string
+    add_column :annual_evaluations, :neuro_zone_of_preservation2, :string
+    add_column :annual_evaluations, :neuro_zone_of_preservation3, :integer
+
+    add_column :annual_evaluations, :level_of_injury, :integer
+    add_column :annual_evaluations, :asia_score, :integer
+
+    # Columns not used.
+    remove_column :annual_evaluations, :asia_level
+    remove_column :annual_evaluations, :next_eval
+    remove_column :annual_evaluations, :audit
+    remove_column :annual_evaluations, :cage
+    remove_column :annual_evaluations, :ces_d
+    remove_column :annual_evaluations, :chart_sf
+    remove_column :annual_evaluations, :dast
+    remove_column :annual_evaluations, :dusoi
+    remove_column :annual_evaluations, :dusoi_a
+    remove_column :annual_evaluations, :fam
+    remove_column :annual_evaluations, :kurtzke_fss
+    remove_column :annual_evaluations, :mnfm
+    remove_column :annual_evaluations, :prime_md
+    remove_column :annual_evaluations, :push
+    remove_column :annual_evaluations, :sf_mpq
+    remove_column :annual_evaluations, :sf_8
+    remove_column :annual_evaluations, :swls
+    remove_column :annual_evaluations, :pumt_kit
+    remove_column :annual_evaluations, :colonoscopy_received
+    remove_column :annual_evaluations, :sigmoidoscopy_received
+    remove_column :annual_evaluations, :diabetic_retinal_screening_received
+  end
+end
