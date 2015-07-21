fc9d018cfb97cba88f8032e1db91f041014b4529
Emily Wright-Moore <emily@codeforamerica.org>
2015-07-21 10:13:21 -0700

    radio radio radio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
     options[:html] = { class: 'form-horizontal'}
     options[:wrapper] = :horizontal_input_group
     options[:wrapper_mappings] = {
-      check_boxes: :horizontal_radio_and_checkboxes,
-      radio_buttons: :horizontal_radio_and_checkboxes,
+    #   check_boxes: :horizontal_radio_and_checkboxes,
+    #   radio_buttons: :horizontal_radio_and_checkboxes,
       file: :horizontal_file_input,
       boolean: :horizontal_boolean
     }
