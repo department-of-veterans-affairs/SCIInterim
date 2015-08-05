c8615b9ea28647617beb8276999d2c00fee16802
Emily Wright-Moore <emily@codeforamerica.org>
2015-08-05 10:01:20 -0700

    remove conditional stuff for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,6 @@ module ApplicationHelper
     options[:html] = { class: 'form-vertical'}
     options[:wrapper] = :vertical_input_group
     options[:wrapper_mappings] = {
-    #   check_boxes: :horizontal_radio_and_checkboxes,
-    #   radio_buttons: :horizontal_radio_and_checkboxes,
       file: :horizontal_file_input,
       boolean: :horizontal_boolean
     }
