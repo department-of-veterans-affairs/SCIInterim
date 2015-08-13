a9a48896309f66583514098ddbeb0e5288d94d56
Albert J. Wong <awong.dev@gmail.com>
2015-08-12 23:46:58 -0700

    Hack up fix to state field.
    Use the locale info in the countries gem to hold the state info.
    
    Note that this whole field doesn't necessarily make sense if the country
    is not US.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812235829) do
+ActiveRecord::Schema.define(version: 20150813064319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,12 +51,12 @@ ActiveRecord::Schema.define(version: 20150812235829) do
     t.string   "address1"
     t.string   "address2"
     t.string   "city"
-    t.integer  "state"
     t.integer  "zip"
     t.string   "country"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "phone_number"
+    t.string   "state"
   end
 
   create_table "annual_evaluations", force: true do |t|
