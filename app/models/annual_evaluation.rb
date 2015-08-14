a44bf9acccf7c8a8f037d7152547d2ceb0562c42
Emily Wright-Moore <emily@codeforamerica.org>
2015-08-14 10:37:54 -0700

    add inpatient radio will eventually close #274

--- app/models/annual_evaluation.rb
+++ app/models/annual_evaluation.rb
@@ -7,6 +7,7 @@ class AnnualEvaluation < ActiveRecord::Base
 
   def self.collections
     {
+      is_inpatient: [ {value: true, label: "Inpatient"}, {value: false, label: "Outpatient"} ],
       bladder_drainage_method: Domain::BladderDrainageMethod.collection,
     }.with_indifferent_access
   end
