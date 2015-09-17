ecbf864d09db73062e74a43cb5c9b2ec98aa6d3a
Albert J. Wong <awong.dev@gmail.com>
2015-09-16 17:01:37 -0700

    Auto-generate reports UI based on model.
    Takes the clean readable code and makes it into something with too much
    reflection for its own good.
    
    On the flip side, changes to the database schema are now tied directly
    into this UI so it's impossible to forget to add an option here.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,9 +19,58 @@ class ReportsController < ApplicationController
   end
   helper_method :results
 
-  def export_sections
+  # Takes the set of options and creates a list of section_name/attribute_list
+  # pairs that can be used to render an export configuration page.
+  #
+  # TODO(awong): This incorrectly adds spaces into some of the attribute names.
+  # The "prefix" needs to be the pre-camelcased version.
+  def export_config
+    def squish_options(model, prefix, option_list, attr_list)
+      export_options = []
+      nested_attributes = Set.new(model.nested_attributes_options.keys)
+
+      option_list.each do |key, value|
+        if value.is_a?(Hash)
+          nested_section = key.to_s.split("_").map { |word| word.capitalize }.join(" ")
+          nested_section = "#{prefix}[#{nested_section}]" if prefix != ''
+          association = model.reflect_on_association(key)
+          if association.macro == :has_many || !nested_attributes.include?(key)
+            section_attrlist = []
+            export_options << [nested_section, section_attrlist]
+            export_options.concat(squish_options(association.klass, '', value, section_attrlist))
+          else
+            # This is a has_one or a belongs_to relation and is nested.
+            squish_options(association.klass, nested_section, value, attr_list)
+          end
+        else
+          if prefix.empty?
+            attr_list << key
+          else
+            attr_list << "#{prefix}[#{key}]"
+          end
+        end
+      end
+      export_options
+    end
+
+    patient_attributes = []
+    [['Patient', patient_attributes]].concat(
+      squish_options(Patient, '', all_export_options, patient_attributes))
   end
-  helper_method :export_sections
+  helper_method :export_config
+
+  # Helper to make the view code for the accordian less of a mess.
+  # All this does is return "active" the first time and then an empty
+  # string every other time. This lets us mark the first section active.
+  def returns_active_once
+    if @active_class.nil?
+      @active_class = ""
+      "active"
+    else
+      @active_class
+    end
+  end
+  helper_method :returns_active_once
 
   def all_export_options
     @all_export_options ||= Patient.all_attributes
